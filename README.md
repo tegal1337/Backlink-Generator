a34f70445a22f438b18335719ade255da8176e2a
insideourheart <abdulmuttaqin456@gmail.com>
2023-01-31 06:18:40 +0700

    AD +

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# Backlink-Generator
-mass Backlink Generator
+# Recheck
+
+
+  <p align="center">
+  <image src="https://user-images.githubusercontent.com/31664438/180301192-232392c0-3275-42bd-896c-499f73e7a091.png" align="center"  width="100%"/>
+<h1 align="center"> Recheck </h1>
+Deep scan domain and find all possible domain  to takeover
+    </p>
+    
+## Quick Links
+
+* [Demo](https://www.youtube.com/watch?v=TxvH8B3srXI)
+* [Installation](#run--installation)
+* [Feature](#feature)
+
+# Example 
+![image](https://user-images.githubusercontent.com/31664438/180301953-2e06277e-6a03-4e55-b216-1b0fafcffb2b.png)
+
+
+
+## Run / Installation
+How to run ?
+
+```bash
+> git clone https://github.com/tegal1337/Recheck
+> cd Recheck
+> npm install
+> npm start
+```
+
+
+
+## Feature
+
+ Feature  | Status |
+| -------------:|------------- |
+| Deepscan domain | Ok|
+| Find Possible takeover domain | Ok |
+
+
+## Requirements
+
+ - Windows / Linux
+ - NodeJS v10 or higher
+
+
+## Donate
+
+???
+
+
+
+
+
+           
+
---
+++ index.js
@@ -0,0 +1,81 @@
+const { prompt } = require("enquirer");
+var clc = require("cli-color");
+const {
+  ERR_DOMAIN,
+  BANNER,
+  QUESTION,
+  VALIDATOR,
+  INPUT,
+  CAPTION,
+  FAQ,
+} = require("./lib/constant");
+const { getAPI } = require("./lib/getAPI");
+const { getBacklink } = require("./lib/getBacklink");
+const { checkLinks } = require("./lib/checkLink");
+const cliSpinners = require("cli-spinners");
+const Spinners = require("spinnies");
+const spinners = new Spinners({
+  spinnerColor: "blueBright",
+  succeedColor: "greenBright",
+  failColor: "redBright",
+  spinner: cliSpinners.dots,
+});
+
+const question = [
+  {
+    type: "input",
+    name: INPUT,
+    message: QUESTION,
+    validator: VALIDATOR,
+    warning: ERR_DOMAIN,
+  },
+];
+
+console.log(
+  clc.greenBright(BANNER) +
+    "\n" +
+    clc.blueBright(CAPTION) +
+    "\n" +
+    clc.yellowBright(FAQ) +
+    "\n"
+);
+
+(async function startApp() {
+  const { [INPUT]: domain } = await prompt(question);
+  console.log(clc.whiteBright("\n\n"));
+  const data = await getAPI();
+
+  for (let i = 0; i < data.length; i++) {
+    const link = getBacklink(data[i]);
+    const res = await checkLinks(link, domain);
+    spinners.add("success", {
+      text: " [+] Starting [+]",
+      color: "green",
+    });
+    switch (res) {
+      case 200:
+        let succes = "[+] " + link + domain + " [200]";
+        spinners.update("success", {
+          text: succes,
+          color: "green",
+        });
+        break;
+      case 404:
+        let notfound = "[-] " + link + domain + " [404]";
+        spinners.add("err", {
+          text: notfound,
+          color: "red",
+        });
+
+        spinners.update("err", {
+          text: notfound,
+          color: "red",
+        });
+        break;
+      default:
+        let unknown = "[?] " + link + domain + " [Unknown]";
+        console.log(clc.whiteBright(unknown));
+        break;
+    }
+  }
+})();
